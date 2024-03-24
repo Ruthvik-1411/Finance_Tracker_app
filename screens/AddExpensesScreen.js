85db1804d8edfbbccb87c3376ccd2d61ae6157b7
Ruthvik-1411 <nagasairuthvik1919@gmail.com>
2024-03-24 17:33:05 +0530

    prettier js code

--- screens/AddExpensesScreen.js
+++ screens/AddExpensesScreen.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { Dimensions, StyleSheet } from 'react-native';
-import { Card, TextInput } from 'react-native-paper';
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { Picker } from '@react-native-picker/picker';
+import React, { useState } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+import { Card, TextInput } from "react-native-paper";
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
 
-import { CategoryModal, categoriesData } from './utils/CategoryModal';
+import { CategoryModal, categoriesData } from "./utils/CategoryModal";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const AddExpenseScreen = () => {
   const [showDatePicker, setShowDatePicker] = React.useState(false);
   const [date, setDate] = React.useState(new Date());
-  const [amount, setAmount] = useState('');
-  const [description, setDescription] = useState('');
-  const [notes, setNotes] = useState('');
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
+  const [notes, setNotes] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedbin, setSelectedbin] = React.useState("Want"); //can be a date or final password
-  const [selectedTxtype, setSelectedTxtype] = React.useState("UPI");//to handle password expiry
+  const [selectedTxtype, setSelectedTxtype] = React.useState("UPI"); //to handle password expiry
 
   const toggleModal = () => {
     setShowModal(!showModal);
@@ -33,8 +33,8 @@ const AddExpenseScreen = () => {
   };
 
   const formatDate = (date) => {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   };
@@ -47,11 +47,11 @@ const AddExpenseScreen = () => {
 
   const handleSaveExpense = () => {
     // Call your backend function here to save the expense
-    if (amount !== '') {
+    if (amount !== "") {
       // saveExpense({ amount, category: selectedCategory });
       // navigation.goBack();
     } else {
-      alert('Please enter an amount!');
+      alert("Please enter an amount!");
     }
   };
 
@@ -63,7 +63,7 @@ const AddExpenseScreen = () => {
     setSelectedCategory(null);
     setSelectedbin("Want");
     setSelectedTxtype("UPI");
-  }
+  };
 
   return (
     <View style={styles.container}>
@@ -73,36 +73,44 @@ const AddExpenseScreen = () => {
             <Text style={styles.entryTitle}>New Expense </Text>
             <Icon name="wallet-plus-outline" size={35} color="#000000" />
           </View>
-          <Card
+          <View
             style={{
-              width: 140,
-              //marginHorizontal: 200,
-              alignItems: 'center',
-              elevation: 5,
-            }}>
-            <View
+              justifyContent: "center",
+            }}
+          >
+            <Card
               style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}>
-              <Text>{formatDate(date).toString()}</Text>
-              <Button
-                onPress={() => setShowDatePicker(true)}
-                icon={<Icon name="chevron-down" size={25} color={'black'} />}
-                type="clear"
-              />
-              {showDatePicker && (
-                <DateTimePicker
-                  testID="dateTimePicker"
-                  value={date}
-                  mode={'date'}
-                  is24Hour={true}
-                  display="default"
-                  onChange={onChange}
+                width: 140,
+                //marginHorizontal: 200,
+                alignItems: "center",
+                elevation: 5,
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Text>{formatDate(date).toString()}</Text>
+                <Button
+                  onPress={() => setShowDatePicker(true)}
+                  icon={<Icon name="chevron-down" size={25} color={"black"} />}
+                  type="clear"
                 />
-              )}
-            </View>
-          </Card>
+                {showDatePicker && (
+                  <DateTimePicker
+                    testID="dateTimePicker"
+                    value={date}
+                    mode={"date"}
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                  />
+                )}
+              </View>
+            </Card>
+          </View>
           <View style={styles.inputContainer}>
             <TextInput
               label="Amount*"
@@ -113,10 +121,10 @@ const AddExpenseScreen = () => {
               maxLength={11}
               keyboardType="numeric"
               style={styles.input}
-              theme={{ colors: { primary: '#a9a9a9' } }}
+              theme={{ colors: { primary: "#a9a9a9" } }}
             />
-            </View>
-            <View style={styles.inputContainer}>
+          </View>
+          <View style={styles.inputContainer}>
             <TextInput
               label="Description*"
               value={description}
@@ -126,10 +134,10 @@ const AddExpenseScreen = () => {
               multiline={true}
               maxLength={150}
               style={styles.inputdescription}
-              theme={{ colors: { primary: '#a9a9a9' } }}
+              theme={{ colors: { primary: "#a9a9a9" } }}
             />
-            </View>
-            <View style={styles.inputContainer}>
+          </View>
+          <View style={styles.inputContainer}>
             <TextInput
               label="Notes"
               value={notes}
@@ -138,7 +146,7 @@ const AddExpenseScreen = () => {
               mode="flat"
               maxLength={30}
               style={styles.input}
-              theme={{ colors: { primary: '#a9a9a9' } }}
+              theme={{ colors: { primary: "#a9a9a9" } }}
             />
           </View>
           <View>
@@ -147,23 +155,27 @@ const AddExpenseScreen = () => {
                 width: 200,
                 marginHorizontal: 20,
                 marginVertical: 10,
-                alignItems: 'center',
+                alignItems: "center",
                 elevation: 5,
-                justifyContent: 'center',
-                marginTop: 20
-              }}>
+                justifyContent: "center",
+                marginTop: 20,
+              }}
+            >
               <View
                 style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}>
-                <Text style={{
-                  alignItems: 'center',
-                  paddingLeft: 20,
-                  paddingRight: 20,
-                  fontSize: 20
-                }}>
-                  {selectedCategory ? selectedCategory.name : 'Category'}
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    alignItems: "center",
+                    paddingLeft: 20,
+                    paddingRight: 20,
+                    fontSize: 20,
+                  }}
+                >
+                  {selectedCategory ? selectedCategory.name : "Category"}
                 </Text>
                 <Button
                   onPress={toggleModal}
@@ -171,7 +183,7 @@ const AddExpenseScreen = () => {
                     <Icon
                       name="plus-circle-multiple-outline"
                       size={35}
-                      color={'black'}
+                      color={"black"}
                     />
                   }
                   type="clear"
@@ -181,15 +193,17 @@ const AddExpenseScreen = () => {
           </View>
           <View
             style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center'
-            }}>
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <View style={styles.dropdownContainer}>
               <Picker
                 selectedValue={selectedbin}
                 style={styles.dropdownbin}
-                onValueChange={(itemValue) => setSelectedbin(itemValue)}>
+                onValueChange={(itemValue) => setSelectedbin(itemValue)}
+              >
                 <Picker.Item label="Want" value="want" />
                 <Picker.Item label="Need" value="need" />
                 <Picker.Item label="Required" value="required" />
@@ -199,7 +213,8 @@ const AddExpenseScreen = () => {
               <Picker
                 selectedValue={selectedTxtype}
                 style={styles.dropdowntx}
-                onValueChange={(itemValue) => setSelectedTxtype(itemValue)}>
+                onValueChange={(itemValue) => setSelectedTxtype(itemValue)}
+              >
                 <Picker.Item label="UPI" value="upi" />
                 <Picker.Item label="Cash" value="cash" />
                 <Picker.Item label="Debit Card" value="debitcard" />
@@ -212,12 +227,12 @@ const AddExpenseScreen = () => {
             <Button
               title="Clear"
               titleStyle={{
-                color: '#ffffff',
+                color: "#ffffff",
                 fontSize: 18,
-                fontWeight: 'bold',
+                fontWeight: "bold",
               }}
               buttonStyle={{
-                backgroundColor: '#000000',
+                backgroundColor: "#000000",
                 borderRadius: 20,
                 marginTop: 40,
                 marginLeft: 20,
@@ -230,12 +245,12 @@ const AddExpenseScreen = () => {
             <Button
               title="Save"
               titleStyle={{
-                color: '#ffffff',
+                color: "#ffffff",
                 fontSize: 18,
-                fontWeight: 'bold',
+                fontWeight: "bold",
               }}
               buttonStyle={{
-                backgroundColor: '#000000',
+                backgroundColor: "#000000",
                 borderRadius: 20,
                 marginTop: 40,
                 marginLeft: 50,
@@ -260,19 +275,19 @@ const AddExpenseScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ffffff',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ffffff",
     paddingBottom: 10,
   },
   scrollContent: {
     flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   hstack: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   card: {
     width: windowWidth * 0.9,
@@ -281,10 +296,10 @@ const styles = StyleSheet.create({
     elevation: 5,
     margin: 5,
     padding: 10,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000000',
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -293,14 +308,14 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
   },
   entryContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 1,
   },
   entryTitle: {
     fontSize: 25,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginLeft: 10,
   },
   inputContainer: {
@@ -308,15 +323,15 @@ const styles = StyleSheet.create({
     height: 65,
   },
   input: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     marginTop: 5,
     fontSize: 14,
   },
   inputdescription: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     marginTop: 5,
     fontSize: 14,
-    height: 80
+    height: 80,
   },
   dropdownContainer: {
     marginTop: 15,
@@ -325,15 +340,15 @@ const styles = StyleSheet.create({
     width: 145,
     height: 20,
     borderWidth: 1,
-    borderColor: '#a9a9a9',
+    borderColor: "#a9a9a9",
     borderRadius: 5,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   dropdowntx: {
     width: 170,
     height: 20,
     borderWidth: 1,
-    borderColor: '#a9a9a9',
+    borderColor: "#a9a9a9",
     borderRadius: 5,
   },
 });
