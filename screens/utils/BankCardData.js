0298ecc1aa772b684f8bf8730bcf493f1d7a4c7e
Ruthvik-1411 <nagasairuthvik1919@gmail.com>
2024-06-09 15:40:10 +0530

    default static card data in bankcarddata

---
+++ screens/utils/BankCardData.js
@@ -0,0 +1,87 @@
+export const cardDetails = {
+  hdfc: {
+    credit: {
+      category: "Platinum Credit",
+      banklogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/hdfc_bank_logo_.png?raw=true",
+      number: "1234 5678 9012 3456",
+      validity: "12/24",
+      cvv: "123",
+      holdername: "Robert Ford",
+      gatewaylogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/visa_logo.png?raw=true",
+    },
+    debit: {
+      category: "Platinum Debit",
+      banklogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/hdfc_bank_logo_.png?raw=true",
+      number: "1234 5678 9012 3456",
+      validity: "12/24",
+      cvv: "123",
+      holdername: "Dolores N. Abernathy",
+      gatewaylogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/mastercard_logo.png?raw=true",
+    },
+  },
+  sbi: {
+    debit: {
+      category: "Diamond Debit",
+      banklogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/State_Bank_of_India_logo.png?raw=true",
+      number: "1234 5678 9012 3456",
+      validity: "12/24",
+      cvv: "234",
+      holdername: "Arnold Thomas",
+      gatewaylogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/rupay_logo.png?raw=true",
+    },
+  },
+  icici: {
+    credit: {
+      category: "Golden Credit",
+      banklogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/icici_bank_logo.png?raw=true",
+      number: "1234 5678 9012 3456",
+      validity: "12/24",
+      cvv: "345",
+      holdername: "Peter Parker",
+      gatewaylogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/visa_logo.png?raw=true",
+    },
+    debit: {
+      category: "Golden Debit",
+      banklogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/icici_bank_logo.png?raw=true",
+      number: "1234 5678 9012 3456",
+      validity: "12/24",
+      cvv: "456",
+      holdername: "Peter Parker",
+      gatewaylogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/visa_logo.png?raw=true",
+    },
+  },
+  default: {
+    credit: {
+      category: "Platinum Default",
+      banklogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/hdfc_bank_logo_.png?raw=true",
+      number: "1234 5678 9012 3456",
+      validity: "12/24",
+      cvv: "123",
+      holdername: "Dolores N. Abernathy",
+      gatewaylogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/visa_logo.png?raw=true",
+    },
+    debit: {
+      category: "Platinum Default",
+      banklogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/hdfc_bank_logo_.png?raw=true",
+      number: "1234 5678 9012 3456",
+      validity: "12/24",
+      cvv: "123",
+      holdername: "Dolores N. Abernathy",
+      gatewaylogo:
+        "https://github.com/Ruthvik-1411/Finance_Tracker_app/blob/main/assets/card_assets/visa_logo.png?raw=true",
+    },
+  },
+};
